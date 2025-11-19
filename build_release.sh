96a7fd33ef3a0fadef72d3bbfac223257bf08b1b
jcy <89929647+annajcy@users.noreply.github.com>
2025-11-19 16:23:42 +0800

    add build release

---
+++ build_release.sh
@@ -0,0 +1,6 @@
+conan build . \
+            -s build_type=Release \
+            -s compiler.cppstd=17 \
+            -c tools.system.package_manager:mode=install \
+            -c tools.system.package_manager:sudo=True \
+            --build=missing
